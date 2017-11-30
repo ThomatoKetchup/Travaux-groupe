ea1a6cca23cc73394102c679bbbe1a25bc5285e7
ThomatoKetchup <33425389+ThomatoKetchup@users.noreply.github.com>
2017-11-30 16:29:02 +0100

    Change

--- DM selenium 3/user-extensions.js
+++ DM selenium 3/user-extensions.js
@@ -3,7 +3,7 @@ var ChaineAleatoire = function ChaineAleatoire(min,max)
 	
 	var ListeCar = new Array();
 	//var ListeCar = new Array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9");
-	for(i=21;i<127;i++){
+	for(i=32;i<127;i++){
 		ListeCar.push(String.fromCharCode(i));
 	}
 	var Chaine ='';
@@ -17,12 +17,14 @@ var ChaineAleatoire = function ChaineAleatoire(min,max)
 
 var Bool = function Bool()
 {
-	var ListeCar = new Array("Oui","Non");
-	var Chaine = '';
-	Chaine = ListeCar[Math.floor(Math.random()*ListeCar.length)];
-	return Chaine;
+	if(Math.random() <0.5)
+			return 'Oui';
+		else
+			return 'Non';
+
 }
 
+	
 var Nombre = function Nombre(nbcar)
 {
 	var ListeCar = new Array("0","1","2","3","4","5","6","7","8","9");
@@ -37,5 +39,16 @@ var Nombre = function Nombre(nbcar)
 
 function getRandomLength(min, max) {
   return (Math.random() * (max - min) + min);
+
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getDate(){
+	var Chaine = getRandomInt(1,31)+'-'+getRandomInt(1,12)+'-'+getRandomInt(1900,2020);
+	return Chaine;
+}
